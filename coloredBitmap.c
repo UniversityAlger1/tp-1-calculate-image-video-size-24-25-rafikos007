a68ff1f8ce7fb027bdb7b772d666c0cbb1df4349
Rifkos007 <rafikmouaici2003@gmail.com>
2024-11-18 20:21:41 +0100

    1st commit

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -6,9 +6,18 @@
 //   u: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
 //   colored image size Bitmap (based on the unit passed parametter)
+
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
 
+long totalSize = (long)w * h * 3; 
+
+    if (strcmp(unit, "bt") == 0) return totalSize;                  
+    if (strcmp(unit, "ko") == 0) return totalSize / 1024.0;             
+    if (strcmp(unit, "mo") == 0) return totalSize / (1024.0 * 1024);    
+    if (strcmp(unit, "go") == 0) return totalSize / (1024.0 * 1024 * 1024);
+
+    
    // YOUR CODE HERE - END
    return 0;
 }
--- video.c
+++ video.c
@@ -1,16 +1,28 @@
-#include <string.h>
-#include "config/video.h"
-// Parameters:
-//   w: width of the image
-//   h: height of the image
-//   durationMovie: duration in second of movie (colored image)
-//   durationCredits: duration in second of credit (image Black/White)
-//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
-// Return value
-//   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-   // YOUR CODE HERE - BEGIN
 
-   // YOUR CODE HERE - END
-   return 0;
+    // Size of the colored movie
+
+    int totalFramesMovie = durationMovie * fps;
+    int totalFramesCredits = durationCredits * fps;
+    int totalPixels1 = w * h * 3;
+    int totalPixels2 = w * h ;
+    int movieSizeBytes = totalFramesMovie * totalPixels1  ; 
+    int creditsSizeBytes = totalFramesCredits * totalPixels2 ; 
+    int totalSize = movieSizeBytes + creditsSizeBytes ;
+
+    // Convertion
+    float size;
+    if (strcmp(unit, "bt") == 0) {
+        size = totalSize;
+    } else if (strcmp(unit, "ko") == 0) {
+        size = totalSize / 1024.0f; // 1 KB = 1024 bytes
+    } else if (strcmp(unit, "mo") == 0) {
+        size = totalSize / (1024.0f * 1024.0f); // 1 MB = 1024 KB
+    } else if (strcmp(unit, "go") == 0) {
+        size = totalSize / (1024.0f * 1024.0f * 1024.0f); // 1 GB = 1024 MB
+    } else {
+        return 0; 
+    }
+
+    return size;
 }
